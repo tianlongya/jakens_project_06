92f6ae8a37d02ae0e2a6beaff42929caf4262559
hello <18331290206@163.com>
2020-07-12 15:14:32 +0800

    first commit

---
+++ scripts/test_login.py
@@ -0,0 +1,15 @@
+class TestLogin:
+    def test_ysh(self):
+        assert 1
+
+    def test_ysh01(self):
+        assert 1
+
+    def test_ysh02(self):
+        assert 1
+
+    def test_ysh03(self):
+        assert 1
+
+    def test_ysh04(self):
+        assert 1
